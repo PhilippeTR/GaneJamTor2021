74e6035100a308bc88ad2a96db725dfe3cbc5378
Nivek <kevin.duguay.1@ens.etsmtl.ca>
2021-05-14 19:49:40 -0400

    Added utilities scripts, health script and obstacle tag

---
+++ Assets/Scripts/Character/Health.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public interface IHealthSubscriber
+{
+    void NotifyHealthChange(Health healthScript, int health);
+    void NotifyHealthDepleted(Health healthScript);
+}
+
+public class Health : MonoSubscribable<IHealthSubscriber>
+{
+    [Header("Health")]
+    [SerializeField]
+    private int _maxHealth = 1;
+    public int MaxHealth
+    {
+        get { return _maxHealth; }
+        private set { _maxHealth = value; }
+    }
+
+    public int HealthPoint { get; protected set; }
+
+    private void Awake()
+    {
+        HealthPoint = MaxHealth;
+    }
+
+    public void ModifyHealth(int damage)
+    {
+        int previousHealthPoint = HealthPoint;
+        Mathf.Clamp(HealthPoint -= damage, 0, MaxHealth);
+
+        if (previousHealthPoint != HealthPoint)
+        {
+            // Warn subscribers that health changed
+            foreach (IHealthSubscriber subscriber in Subscribers)
+            {
+                subscriber.NotifyHealthChange(this, HealthPoint);
+            }
+
+            if (HealthPoint <= 0)
+            {
+                // Warn subscribers that health is depleted
+                foreach (IHealthSubscriber subscriber in Subscribers)
+                {
+                    subscriber.NotifyHealthDepleted(this);
+                }
+            }
+        }
+    }
+}
