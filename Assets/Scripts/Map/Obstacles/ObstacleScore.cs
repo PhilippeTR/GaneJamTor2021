03bb9e23a750fba683c60571c900ba7bf204be96
Nivek <kevin.duguay.1@ens.etsmtl.ca>
2021-05-16 12:31:33 -0400

    Updated obstacle templates

--- Assets/Scripts/Map/Obstacles/ObstacleScore.cs
+++ Assets/Scripts/Map/Obstacles/ObstacleScore.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 public class ObstacleScore : MonoBehaviour
 {
     ObstacleParent parent;
+    bool triggered = false;
+
     private void Start()
     {
         parent = GetComponentInParent<ObstacleParent>();
     }
+
     private void OnTriggerExit(Collider other)
     {
-        
-        if (other.CompareTag("Player"))
+        if (!triggered && other.CompareTag("Player"))
         {
             if (parent)
             {
                 parent.ObstaclePassed();
+                triggered = true;
             }
         }
     }
