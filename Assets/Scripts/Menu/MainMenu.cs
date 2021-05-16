145ca10d5aeedb2d4ad64a60516babaa25ec6380
Jlim <jimmyetlim@gmail.com>
2021-05-16 04:22:04 -0400

    Main Menu push

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
@@ -15,4 +16,19 @@ public class MainMenu : MonoBehaviour
     {
         
     }
+
+    public void LoadScene(string scenename)
+    {
+        SceneManager.LoadScene(scenename);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+         UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
 }
